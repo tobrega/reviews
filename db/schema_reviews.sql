b5232cde99bfdf375918210861cae9bc6ba0c2ea
Tony Ly <tonyjly@gmail.com>
2021-05-07 00:57:30 -0700

    update data types

--- db/schema_reviews.sql
+++ db/schema_reviews.sql
@@ -1,16 +1,23 @@
 -- Create table to hold raw imported data --
 
+LOGIN: psql -d reviews -U tony -W
+
+sudo docker run -p 5434:80 \
+   -e 'PGADMIN_DEFAULT_EMAIL=user@domain.com' \
+   -e 'PGADMIN_DEFAULT_PASSWORD=student' \
+   -d dpage/pgadmin4
+
 DROP TABLE IF EXISTS reviews;
 
 CREATE TABLE reviews (
   id              INTEGER primary key,
   product_id      INTEGER,
-  rating          INTEGER,
+  rating          VARCHAR(255),
   date            VARCHAR(255),
-  summary         VARCHAR(255),
+  summary         VARCHAR(510),
   body            VARCHAR(510),
-  recommend       BOOLEAN,
-  reported        BOOLEAN,
+  recommend       VARCHAR,
+  reported        VARCHAR,
   reviewer_name   VARCHAR(255),
   reviewer_email  VARCHAR(255),
   response        VARCHAR(255),
@@ -19,6 +26,5 @@ CREATE TABLE reviews (
 
 -- Import data --
 COPY reviews
-FROM '/home/tony/Nextcloud/HR-SEA16/sdc/data-jelder/reviews.csv'
--- WITH (format csv, header)
-DELIMITER ',' CSV HEADER;
+FROM '/home/tony/Nextcloud/HR-SEA16/sdc/tobrega-reviews/db/reviewsCleaned.csv'
+WITH (format csv, header)
