5346ce1a5cd72b5b47dcfb4114114ec447c33a54
Tony Ly <tonyjly@gmail.com>
2021-05-08 10:39:35 -0700

    consolidate schema into one file

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS reviews, characteristics;
 
 CREATE TABLE reviews (
   id              INTEGER NOT NULL primary key,
@@ -7,13 +7,20 @@ CREATE TABLE reviews (
   date            VARCHAR(255),
   summary         VARCHAR(510),
   body            VARCHAR(510),
-  recommend       VARCHAR,
-  reported        VARCHAR,
+  recommend       BOOLEAN,
+  reported        BOOLEAN,
   reviewer_name   VARCHAR(255),
   reviewer_email  VARCHAR(255),
   response        VARCHAR(255),
   helpfulness     INTEGER
 );
 
+CREATE TABLE characteristics (
+  id          INTEGER NOT NULL primary key,
+  product_id  INTEGER,
+  name        VARCHAR(50)
+);
+
 -- Import data --
 COPY reviews FROM '/home/tony/Nextcloud/HR-SEA16/sdc/tobrega-reviews/db/reviewsCleaned.csv' WITH (format csv, header)
+COPY characteristics FROM '/home/tony/Nextcloud/HR-SEA16/sdc/tobrega-reviews/db/characteristicsCleaned.csv' WITH (format csv, header)
