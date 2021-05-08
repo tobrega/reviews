43d2ce09f91b8e36b0d1178323e2a7d11464fa66
Tony Ly <tonyjly@gmail.com>
2021-05-08 00:12:54 -0700

    decrease varchar limit, update filename

--- db/schema_characteristics.sql
+++ db/schema_characteristics.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS characteristics;
 CREATE TABLE characteristics (
   id          INTEGER primary key,
   product_id  INTEGER,
-  name        VARCHAR(255)
+  name        VARCHAR(50)
 );
 
 -- Import data --
-COPY characteristics FROM '/home/tony/Nextcloud/HR-SEA16/sdc/tobrega-reviews/db/characteristics.csv' WITH (format csv, header)
+COPY characteristics FROM '/home/tony/Nextcloud/HR-SEA16/sdc/tobrega-reviews/db/characteristicsCleaned.csv' WITH (format csv, header)
