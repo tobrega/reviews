cc9c4f2fcd3f5d9acc3fac4918378a8fe900b656
Tony Ly <tonyjly@gmail.com>
2021-05-10 23:36:12 -0700

    add db config example

---
+++ db/config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  host: 'localhost',
+  port: 5432,
+  database: 'reviews',
+  user: 'user',
+  password: 'hunter2',
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+}
