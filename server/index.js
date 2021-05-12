5bbd08496f156c6184bb07f2e6f3808a63fe79c9
Tony Ly <tonyjly@gmail.com>
2021-05-11 23:56:19 -0700

    add additional server endpoints

--- server/index.js
+++ server/index.js
@@ -12,8 +12,14 @@ app.get('/', (req, res) => { res.status(200).send('Connect to endpoint at /revie
 // REVIEWS ENDPOINT
 app.get('/reviews', router)
 
-// PARAM: product_id
+// GET reviews by product_id
 app.get('/reviews/:product_id', router);
+
+// GET metadata
+app.get('/reviews/meta/:product_id', router);
+
+// POST review by product_id
+app.post('/reviews/:product_id', router);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
