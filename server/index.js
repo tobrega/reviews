226a6885004206146139892443bd58f32bae7056
Tony Ly <tonyjly@gmail.com>
2021-05-13 00:39:57 -0700

    switch to app.use and redirect to routes

--- server/index.js
+++ server/index.js
@@ -1,28 +1,33 @@
 const express = require('express');
+const morgan = require('morgan');
 const db = require('../db/index.js');
 const router = require('./routes.js');
 
 const app = express();
 const port = 3000;
+app.use(morgan('dev'));
 app.use(express.json());
 
 // EXTERNAL ENDPOINT
-app.get('/', (req, res) => { res.status(200).send('Connect to endpoint at /reviews') })
+// app.use('/', (req, res) => { res.status(200).send('Connect to endpoint at /reviews') })
 
 // REVIEWS ENDPOINT
-app.get('/reviews', router)
+app.use('/reviews', router);
 
-// GET reviews by product_id
-app.get('/reviews/:product_id', router);
+// // GET reviews by product_id
+// app.get('/reviews/:productId', router);
 
-// GET metadata
-app.get('/reviews/meta/:product_id', router);
+// // GET metadata
+// app.get('/reviews/meta/:product_id', router);
 
-// POST review by product_id
-app.post('/reviews/:product_id', router);
+// // POST review by product_id
+// app.post('/reviews/:product_id', router);
+
+// // PUT helpful
+// app.put('/reviews/:product_id', router);
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 
-module.exports = { app };
+// module.exports = { app };
