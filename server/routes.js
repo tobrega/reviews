01243769e81a693b6aca14bcfa63707fa80ce2c6
Tony Ly <tonyjly@gmail.com>
2021-05-13 14:39:19 -0700

    update routes

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ const db = require('../db');
 const router = express.Router();
 
 // REVIEWS
-router.get('/reviews', (req, res) => {
+router.get('/', (req, res) => {
   const query = 'SELECT * FROM reviews LIMIT 10';
   db.query(query, (err, results) => {
     if (err) { res.status(500).send('Error retrieving /reviews', err); }
@@ -13,14 +13,35 @@ router.get('/reviews', (req, res) => {
   })
 });
 
+// // PRODUCT_ID
+// router.get('/reviews/:product_id', (req, res) => {
+//   const { product_id } = req.params;
+//   const query = `SELECT * FROM reviews WHERE product_id = ${product_id}`;
+//   db.query(query, (err, results) => {
+//     if (err) { res.status(500).send('Error retrieving /reviews', err); }
+//     res.status(200).send(results.rows)
+//   })
+// });
+
 // PRODUCT_ID
-router.get('/reviews/:product_id', (req, res) => {
-  const { product_id } = req.params;
-  const query = `SELECT * FROM reviews WHERE product_id = ${product_id}`;
-  db.query(query, (err, results) => {
-    if (err) { res.status(500).send('Error retrieving /reviews', err); }
-    res.status(200).send(results.rows)
-  })
+router.get('/:productId', async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const [ reviewsData, photosData ] = await Promise.all([ // Promise.all takes an array of promises as input
+      db.query('SELECT * FROM reviews WHERE product_id = $1', [productId]),
+      // db.query('SELECT * FROM photos WHERE product_id = $1', [productId]),
+      db.query('SELECT * FROM photos WHERE review_id = $1', [productId])
+    ]);
+    const reviews = reviewsData.rows;
+    const photos = photosData.rows;
+    const results = { reviews, photos }
+    // console.log('results:', reviews, photos)
+    res.status(200).send(results)
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+
 });
 
 module.exports = router;
