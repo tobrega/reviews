066f9ae3165e6028fe78c5e81a68ca34c5c28d6b
Tony Ly <tonyjly@gmail.com>
2021-05-14 00:13:27 -0700

    implement query for reviews results (mostly complete)

--- server/routes.js
+++ server/routes.js
@@ -5,43 +5,58 @@ const db = require('../db');
 const router = express.Router();
 
 // REVIEWS
-router.get('/', (req, res) => {
-  const query = 'SELECT * FROM reviews LIMIT 10';
-  db.query(query, (err, results) => {
-    if (err) { res.status(500).send('Error retrieving /reviews', err); }
-    res.status(200).send(results.rows)
-  })
-});
-
-// // PRODUCT_ID
-// router.get('/reviews/:product_id', (req, res) => {
-//   const { product_id } = req.params;
-//   const query = `SELECT * FROM reviews WHERE product_id = ${product_id}`;
+// router.get('/', (req, res) => {
+//   const query = 'SELECT * FROM reviews LIMIT 10';
 //   db.query(query, (err, results) => {
 //     if (err) { res.status(500).send('Error retrieving /reviews', err); }
 //     res.status(200).send(results.rows)
 //   })
 // });
 
-// PRODUCT_ID
-router.get('/:productId', async (req, res) => {
+// REVIEWS
+router.get('/', async (req, res) => {
+  const { product_id } = req.query;
+  console.log('productId', product_id)
   try {
-    const { productId } = req.params;
-    const [ reviewsData, photosData ] = await Promise.all([ // Promise.all takes an array of promises as input
-      db.query('SELECT * FROM reviews WHERE product_id = $1', [productId]),
-      // db.query('SELECT * FROM photos WHERE product_id = $1', [productId]),
-      db.query('SELECT * FROM photos WHERE review_id = $1', [productId])
-    ]);
-    const reviews = reviewsData.rows;
-    const photos = photosData.rows;
-    const results = { reviews, photos }
-    // console.log('results:', reviews, photos)
-    res.status(200).send(results)
+    const query = `
+      SELECT id,
+        rating,
+        summary,
+        recommend,
+        response,
+        body,
+        date,
+        reviewer_name,
+        reviewer_email,
+        helpfulness,
+        (
+          SELECT ARRAY_TO_JSON(ARRAY_AGG(ROW_TO_JSON(p)))
+          FROM (
+              SELECT id,
+                url
+              FROM photos
+              WHERE r.id = photos.review_id
+            ) p
+        ) AS photos,
+        reported
+      FROM reviews r
+      WHERE product_id = $1;
+    `;
+
+    // json_array_agg
+    const results = await db.query(query, [product_id]);
+
+    // TODO: Use COALESCE to handle when photos array === null
+    results.rows.map((row) => {
+      if (row.photos === null) { row.photos = []; }
+    })
+
+    console.log(results.rows);
+    res.status(200).send(results.rows)
   } catch (err) {
     console.error(err);
     throw err;
   }
-
 });
 
 module.exports = router;
