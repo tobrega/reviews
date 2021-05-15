9d2263cb8d0edcae8a5f4092a724d896ee5c41fb
Tony Ly <tonyjly@gmail.com>
2021-05-15 01:04:18 -0700

    add percentile testing, thanks Brenton!

--- test/script.js
+++ test/script.js
@@ -3,10 +3,17 @@ import { sleep } from 'k6';
 
 export let options = {
   vus: 10,
-  duration: '10s',
+  duration: '30s',
 }
 
-export default function () {
-  http.get('http://localhost:3000/reviews?product_id=20100');
+export default () => {
+  const targetPercentiles = [0.05, 0.5, 0.95];
+  const range = 0.05;
+  const max = 1000000;
+
+  targetPercentiles.forEach((percentile) => {
+    const random = Math.floor(max * range * (Math.random() + percentile / range - 0.5));
+    http.get(`http://localhost:3000/reviews?product_id=${random}`);
   sleep(1);
-}
+  });
+};
